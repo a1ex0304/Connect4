b07549e7e8acc17288fd8dbcb0f883cc9cd50bf3
a1ex0304 <145505974+a1ex0304@users.noreply.github.com>
2024-12-14 00:02:45 -0500

    Update Thanks.java

--- Thanks.java
+++ Thanks.java
@@ -1,1 +1,17 @@
-
+//Import java swing methods
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+public class Thanks extends JFrame{
+  public void Thanks(){
+    JFrame frame = new JFrame();
+    //Make new label
+    JLabel label = new JLabel("Thanks for Playing");
+    //Set the bounds, size, layout, etc
+    label.setBounds(175,200,200,40);
+    frame.add(label);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(530,420);
+    frame.setLayout(null);
+  	frame.setVisible(true);
+  }
+}
